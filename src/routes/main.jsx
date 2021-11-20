b89c88ff6b0bad1f500c36ce74740e70c98dbbf2
JBG0000 <practice9090@naver.com>
2021-11-20 23:19:41 +0900

    [refact] 페이지 글 css 조정

--- src/routes/main.jsx
+++ src/routes/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import SelectOption from "../components/common/selectOption/selectOption";
 import NavBar from "../components/common/navBar/navBar";
 import NaverLogin from "../components/loginPage/login/naverLogin";
+import styles from "./routes.module.css";
 
 function Main() {
   return (
     <>
-      <div style={{ height: 50 }}></div>
-      <NavBar></NavBar>
-      <SelectOption></SelectOption>
+      <div className={styles.body} >
+        <div style={{ height: 50 }}></div>
+        <NavBar></NavBar>
+        <SelectOption></SelectOption>
+      </div>
     </>
   );
 }
